532bffbfe832b519866ef358ac15ca49c74e42d2
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-03-09 18:05:23 -0300

    fix: Accessibility labels

--- Scrumdinger/Views/CardView.swift
+++ Scrumdinger/Views/CardView.swift
@@ -12,13 +12,17 @@ struct CardView: View {
 
 	var body: some View {
 		VStack(alignment: .leading) {
-			Text(scrum.title).font(.headline)
+			Text(scrum.title)
+				.font(.headline)
+				.accessibilityAddTraits(.isHeader)
 			Spacer()
 			HStack {
 				Label("\(scrum.attendees.count)", systemImage: "person.3")
+					.accessibilityLabel("\(scrum.attendees.count) attendees")
 				Spacer()
 				Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-					.padding(.trailing, 20)
+					.labelStyle(.trailingIcon)
+					.accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
 			}
 			.font(.caption)
 		}
