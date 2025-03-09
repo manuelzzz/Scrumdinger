a35216094567716c0e4caa2397852943b1aa2483
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-03-09 14:57:45 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Manuel Santos Souza on 09/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
