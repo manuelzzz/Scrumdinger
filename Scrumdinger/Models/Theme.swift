12e6255fd060542069cc373aa9867db338c611b8
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-03-10 14:59:39 -0300

    chore: Adding ID and CaseIterable to Theme model

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-enum Theme: String {
+enum Theme: String, CaseIterable, Identifiable {
 	case bubblegum
 	case buttercup
 	case indigo
@@ -35,8 +35,12 @@ enum Theme: String {
 	var name: String {
 		rawValue.capitalized
 	}
-	
+
 	var mainColor: Color {
 		Color(rawValue)
+	}
+	
+	var id: String {
+		name
 	}
 }
